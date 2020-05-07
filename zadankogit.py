61a47296745c3d6cbb6a1bf8c205a1b4e5fbfa8f
Oliwia <64958530+oliwiazxc@users.noreply.github.com>
2020-05-07 13:05:40 +0200

    Dodanie komentarzy
    Przy linijkach kodu zostały dodane komentarze, które wyjaśniają, co dokładnie robi każda z nich.

--- zadankogit.py
+++ zadankogit.py
@@ -1,7 +1,7 @@
 def ostatni_element(lista):
-    return lista[-1] # zwróć ostatni element z tej listy, która zostanie podana niżej
+    return lista[-1] # it returns the last element from list, which will be given below
 
-wyraz = input("Wpisz losowy wyraz, by utworzyć z niego listę: ")
-lista = list(wyraz)
+wyraz = input("Wpisz losowy wyraz, by utworzyć z niego listę: ") # allows to create your own list
+lista = list(wyraz) # shows created list
 wynik = ostatni_element(lista)
 print(wynik)
